921e1c7de2c8de5bc901401cf0a96b0b8ccd5919
Leo.Lee <Leo.Lee@xuenn.com>
2018-02-21 13:08:37 +0800

    using element generate layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,131 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <div>
+      <el-container>
+        <el-header>
+          <h1>嘉明湖國家步道</h1>
+          <!--<el-button @click="startHacking">Start</el-button>-->
+          <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+            <el-menu-item index="1"><a href="#div_aside">認識步道</a></el-menu-item>
+            <el-menu-item index="2"><a href="#div_travel">旅遊資訊</a></el-menu-item>
+            <el-menu-item index="3"><a href="#div_trafic">交通資訊</a></el-menu-item>
+            <el-menu-item index="4"><a href="#div_aside_overall">步道導覽</a></el-menu-item>
+            <el-menu-item index="5"><a href="#div_season">季節風貌</a></el-menu-item>
+            <el-menu-item index="6"><a href="#div_enviorment">環境資源</a></el-menu-item>
+            <el-menu-item index="7"><a href="#div_share">民眾分享</a></el-menu-item>
+          </el-menu>
+        </el-header>
+        <el-main>
+        <div id="div_aside">
+          <h2>認識步道</h2>
+          <el-carousel :interval="1000" :autoplay="false" arrow="always">
+            <el-carousel-item v-for="item in 4" :key="item">
+              <h3>{{ item * 10 }}</h3>
+              <h3>{{ item * 20 }}</h3>
+              <h3>{{ item * 30 }}</h3>
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+        <div id="div_travel">
+          <h2>旅遊資訊</h2>
+          <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tab-pane label="行程須知" name="first">
+              <h3>行程須知</h3>
+            </el-tab-pane>
+            <el-tab-pane label="旅程建議" name="second">
+              <h3>旅程建議</h3>
+              <el-carousel :interval="1000" :autoplay="false" arrow="always">
+                <el-carousel-item v-for="item in 4" :key="item">
+                  <h3>{{ item * 10 }}</h3>
+                  <h3>{{ item * 20 }}</h3>
+                  <h3>{{ item * 30 }}</h3>
+                </el-carousel-item>
+              </el-carousel>
+            </el-tab-pane>
+            <el-tab-pane label="手作步道" name="third">
+              <h3>手作步道</h3>
+            </el-tab-pane>
+          </el-tabs>
+        </div>
+        <div id="div_trafic">
+          <h2>交通資訊</h2>
+          <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tab-pane label="自行開車" name="first">自行開車</el-tab-pane>
+            <el-tab-pane label="大眾運輸" name="second">大眾運輸</el-tab-pane>
+          </el-tabs>
+        </div>
+        <div id="div_aside_overall">
+          <h2>步道導覽</h2>
+          <el-carousel :interval="1000" :autoplay="false" arrow="always">
+            <el-carousel-item v-for="item in 4" :key="item">
+              <h3>{{ item * 10 }}</h3>
+              <h3>{{ item * 20 }}</h3>
+              <h3>{{ item * 30 }}</h3>
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+        <div id="div_season">
+          <h2>季節風貌</h2>
+        </div>
+        <div id="div_enviorment">
+          <h2>環境資源</h2>
+        </div>
+        <div id="div_share">
+          <h2>民眾分享</h2>
+        </div>
+        <div>
+          <p>FB</p>
+          <p>G+</p>
+          <p>LINE</p>
+          <p>TW</p>
+          <p>P</p>
+        </div>
+        </el-main>
+      </el-container>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+  methods: {
+    startHacking() {
+      this.$notify({
+        title: "It works!",
+        type: "success",
+        message:
+          "We've laid the ground work for you. It's time for you to build something epic!",
+        duration: 5000
+      });
+    },
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
+  }
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Helvetica, sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 18px;
+  opacity: 0.75;
+  line-height: 300px;
+  margin: 0;
+  width: 33%;
+  float: left;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App.vue'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false;
 
@@ -10,4 +14,29 @@ new Vue({
   el: '#app',
   components: { App },
   template: '<App/>',
+});
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } 
+  });
 });
